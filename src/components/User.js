0845fe121d9f3be692c3b1479231b8f2d40d0490
Matt Frears <matt.frears@gmail.com>
2018-10-24 11:27:14 -0600

    Updates

--- src/components/User.js
+++ src/components/User.js
@@ -4,8 +4,26 @@ class User extends Component {
   constructor(props) {
     super(props);
 
-
+    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    this.props.firebase.auth().signInWithPopup( provider );
   }
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+  }).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+  });
 
   render() {
     return(
