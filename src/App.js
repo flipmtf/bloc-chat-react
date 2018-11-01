5e68c8e3583d94624b69fd007882f961ebc4d171
Matt Frears <matt.frears@gmail.com>
2018-11-01 10:55:14 -0600

    almost done with checkpoint

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
+import User from './components/User';
 
 // Initialize Firebase
 var config = {
@@ -22,11 +23,16 @@ class App extends Component {
     this.state = {
       rooms: [],
       currentRoom: {},
+      user: null,
     }
   }
 
   setActiveRoom(room) {
     this.setState({ currentRoom: room });
+  }
+
+  setUser(user) {
+    this.setState({ user: user });
   }
 
   render() {
@@ -35,6 +41,7 @@ class App extends Component {
         <main>
           <RoomList setActiveRoom={(room) => this.setActiveRoom(room)} activeRoom={this.state.currentRoom} firebase = { firebase } />
           <MessageList firebase = { firebase } setActiveRoom={(room) => this.setActiveRoom(room)} activeRoom={this.state.currentRoom} />
+          <User firebase = { firebase } setUser={(user) => this.setUser(user)} user={this.state.user} />
         </main>
       </div>
     );
--- src/components/User.js
+++ src/components/User.js
@@ -3,31 +3,44 @@ import React, { Component } from 'react';
 class User extends Component {
   constructor(props) {
     super(props);
+    this.login = this.login.bind(this)
+    this.logout = this.logout.bind(this)
 
     const provider = new this.props.firebase.auth.GoogleAuthProvider();
-    this.props.firebase.auth().signInWithPopup( provider );
+    const auth = new this.props.firebase.auth();
   }
 
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-  }).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-  });
+  login() {
+    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    this.props.firebase.auth().signInWithPopup( provider ).then((result) => {
+      this.setState({
+        user: result.user
+      })
+    })
+  }
+
+  logout() {
+    this.props.firebase.auth().signOut().then((result) => {
+      this.setState({
+        user: null
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.props.firebase.auth().onAuthStateChanged( user => {
+      this.props.setUser(user);
+    });
+  }
 
   render() {
+    let authButton = this.props.user ?
+      <button onClick={this.logout}>Log Out</button> :
+      <button onClick={this.login}>Log In</button>
     return(
-
+      <div>
+        {authButton}
+      </div>
     );
   }
 }
